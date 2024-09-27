bd92cc3fbbdb2a8a28286daa6b96a78741686518
Sahal Mohamed <sahal@keyvalue.systems>
2024-09-27 11:31:30 +0530

    create dashboard layout

--- frontend/src/container/dashboard.jsx
+++ frontend/src/container/dashboard.jsx
@@ -1,5 +1,23 @@
-import Card from "../components/Card"
+import Card from "../components/Card";
+import DashboardLayout from "../layout/DashboardLayout";
 
-const Dashboard = () => <div><Card className="w-40 m-10" title="Test">div</Card></div>;
+const Overview = () => (
+  <DashboardLayout>
+    <div className="flex items-center gap-4">
+      <Card className="w-full px-8 text-left" title="TOTAL UPLOADS">
+        <div className="text-xl font-bold">35</div>
+      </Card>
+      <Card className="w-full px-8 text-left" title="TOTAL USER ENGAGED ">
+        <div className="text-xl font-bold">3,525 </div>
+      </Card>
+      <Card className="w-full px-8 text-left" title="TOTAL PROMOTERS">
+        <div className="text-xl font-bold">3,525</div>
+      </Card>
+      <Card className="w-full px-8 text-left" title="TOTAL RETRACTORS">
+        <div className="text-xl font-bold">1,245</div>
+      </Card>
+    </div>
+  </DashboardLayout>
+);
 
-export default Dashboard;
+export default Overview;
---
+++ frontend/src/layout/DashboardLayout.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import logo from "../assets/images/logo.svg";
+
+const DashboardLayout = ({ children }) => {
+  return (
+    <div className="flex flex-col h-full">
+      <div className="flex items-center justify-between px-16 py-4 bg-white">
+        <img src={logo} alt="Picsell logo"/>
+        <div className="bg-[#38456C] flex items-center justify-center size-8 text-white rounded-full">
+          E
+        </div>
+      </div>
+      <div className="grow bg-gray-50 px-16 py-10">{children}</div>
+    </div>
+  );
+};
+
+export default DashboardLayout;
