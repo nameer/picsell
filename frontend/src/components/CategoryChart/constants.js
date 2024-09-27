b4fad48e898b15f4e2b386396f3d19e9456f7533
sidharth-at <sidharth.at@keyvalue.systems>
2024-09-27 19:41:51 +0530

    Bubble chart with non group

---
+++ frontend/src/components/CategoryChart/constants.js
@@ -0,0 +1,17 @@
+export const CHART_CATEGORIES = [
+    {
+      name: 'Category 1',
+      color: '#ff6b6b',
+      subcategories: ['Subcategory 1A', 'Subcategory 1B']
+    },
+    {
+      name: 'Category 2',
+      color: '#4d79ff',
+      subcategories: ['Subcategory 2A', 'Subcategory 2B']
+    },
+    {
+      name: 'Category 3',
+      color: '#4dd79f',
+      subcategories: ['Subcategory 3A']
+    }
+  ];
