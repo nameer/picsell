833e2c58196cb0aeb4085cfdf8c9772084cfa79a
Navaneeth P M <navaneeth.pm@keyvalue.systems>
2024-09-28 00:39:35 +0530

    feat: get Campaign by id

--- backend/app/api/campaign.py
+++ backend/app/api/campaign.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, HTTPException, status
 
 from app import crud
 from app.api.deps import SessionDep
@@ -11,6 +11,15 @@ router = APIRouter()
 def create_campaign(session: SessionDep, data: CampaignCreate) -> Campaign:
     return crud.create_campaign(session, data)
 
-@router.get("",response_model=list[Campaign])
+
+@router.get("", response_model=list[Campaign])
 def get_campaigns(session: SessionDep) -> list[Campaign]:
     return crud.get_campaigns(session)
+
+
+@router.get("/{id}", response_model=Campaign)
+def get_campaign_by_id(id: int, session: SessionDep) -> Campaign:
+    campaign = crud.get_campaign_by_id(session, id)
+    if not campaign:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return campaign
