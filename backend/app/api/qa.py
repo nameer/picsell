6612d53af969cd164c65479febb152f12b5d4747
Nameer PK <nameerpk.95@gmail.com>
2024-09-27 17:46:35 +0530

    feat: Log Q&A:
    - Log from websocket
    - Log from API using bg task

--- backend/app/api/qa.py
+++ backend/app/api/qa.py
@@ -1,19 +1,29 @@
-from fastapi import APIRouter, WebSocket
+from fastapi import APIRouter, BackgroundTasks, WebSocket
 from pydantic import ValidationError
+from sqlmodel import Session
 
 from app import crud
 from app.api.deps import SessionDep
 from app.core import ai
+from app.core.db import engine
 from app.models import EngagementCreate, QAInput, QAOutput, QAQuestion
 
 router = APIRouter()
 
 
+def write_log(session: SessionDep, data: EngagementCreate):
+    crud.create_engagement(session, data)
+
+
 @router.post("", response_model=QAOutput)
-def question_answer(session: SessionDep, data: QAInput) -> dict:
+def question_answer(
+    session: SessionDep,
+    data: QAInput,
+    background_tasks: BackgroundTasks,
+) -> dict:
     response = ai.qa(data.campaign_id, data.session_id, data.question)
     log_data = EngagementCreate(**data.dict(), response=response["message"])
-    crud.create_engagement(session, log_data)
+    background_tasks.add_task(write_log, session, log_data)
     return response
 
 
@@ -22,7 +32,6 @@ async def question_answer_ws(websocket: WebSocket, campaign_id: int, session_id:
     await websocket.accept()
     while True:
         data = await websocket.receive_json()
-        print(campaign_id, type(campaign_id))
         try:
             question = QAQuestion(**data).question
         except ValidationError:
@@ -30,4 +39,12 @@ async def question_answer_ws(websocket: WebSocket, campaign_id: int, session_id:
             continue
         response = ai.qa(campaign_id, session_id, question)
         # TODO: log response
+        log_data = EngagementCreate(
+            campaign_id=campaign_id,
+            session_id=session_id,
+            question=question,
+            response=response["message"],
+        )
+        with Session(engine) as session:
+            write_log(session, log_data)
         await websocket.send_json(response)
