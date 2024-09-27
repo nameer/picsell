4cefeb2c106c57e8a4f6e7906e97b9f36c3d49ae
Nameer <nameer@keyvalue.systems>
2024-09-27 09:33:03 +0530

    feat: Set up micro service

---
+++ chat/app/core/config.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_ignore_empty=True,
+        extra="ignore",
+    )
+
+    PROJECT_NAME: str
+
+
+settings = Settings()
