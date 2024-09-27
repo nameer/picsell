6467fa7143b0fa83dd49072e2a655edd7e9ae345
Sahal Mohamed <sahal@keyvalue.systems>
2024-09-27 14:29:51 +0530

    create chip component

--- frontend/src/container/Overview/consts.jsx
+++ frontend/src/container/Overview/consts.jsx
@@ -1,11 +1,29 @@
 import { DotsHorizontal } from "../../assets/icons";
+import Chip from "../../components/Chip";
+
+const getStatusChipVariant = (status) => {
+  switch (status) {
+    case "Completed":
+      return "success";
+    case "Training in progress":
+      return "warning";
+    default:
+      return "";
+  }
+};
 
 export const projectsTableColumns = [
   { id: "id", heading: "ID", value: (item) => item.id },
   { id: "fileName", heading: "File name", value: (item) => item.fileName },
   { id: "createOn", heading: "Create on", value: (item) => item.createdOn },
   { id: "updatedOn", heading: "Updated On", value: (item) => item.updatedOn },
-  { id: "status", heading: "Status", value: (item) => item.status },
+  {
+    id: "status",
+    heading: "Status",
+    value: (item) => (
+      <Chip text={item.status} variant={getStatusChipVariant(item.status)} />
+    ),
+  },
   { id: "actions", heading: "Actions", value: (item) => <DotsHorizontal /> },
 ];
 
@@ -15,7 +33,7 @@ export const projectsTableData = [
     fileName: "Coffee explainer video ",
     createdOn: "22nd Jan s4",
     updatedOn: "22nd Jan 24",
-    status: "Completed",
+    status: "Training in progress",
   },
   {
     id: "#3251",
